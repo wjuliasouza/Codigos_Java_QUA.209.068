e83c5294004f1bac190aa8432f785a734a364ca4
wjuliasouza <w.juliasouza@gmail.com>
2023-08-02 17:33:01 -0300

    subindo arquivos da primeira aula de java

---
+++ OlaMundo/src/principal/OlaMundo.java
@@ -0,0 +1,13 @@
+package principal;
+
+//classe principal
+public class OlaMundo {
+
+	//metodo principal, onde executa o programa
+	public static void main(String[] args) {
+
+		//imprime uma linha e ao final salta uma nova linha
+		//print = imprimir; ln
+System.out.println("Hello World, ou Olá Mundo!");
+	}
+}
