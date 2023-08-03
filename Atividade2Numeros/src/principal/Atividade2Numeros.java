6c0472e2b37963a6285488d0b5733ac72e465d81
wjuliasouza <w.juliasouza@gmail.com>
2023-08-03 14:37:01 -0300

    Realizada atividade 2 de Java, sobre variáveis

---
+++ Atividade2Numeros/src/principal/Atividade2Numeros.java
@@ -0,0 +1,52 @@
+package principal;
+
+public class Atividade2Numeros {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		/*Escreva duas variáveis (numero1 e numero2) e mostre na tela o resultado
+		 * Faça o mesmo para variáveis tipo caractere/string
+		 * Repita as variáveis do tipo lógico/bool mas recebendo tipo falso e verdadeiro */
+		
+		int numero1 = 5;
+		int numero2 = 10;
+		
+		String nome = "Julia";
+		String sobrenome = "Wanderley";  
+		boolean verdadeiro = true;
+		boolean falso = false;
+		int idade = 27;
+		
+		
+		
+		
+		System.out.println(numero1 + numero2 );
+		System.out.println ("***********************************");
+		System.out.println("Meu nome é: " +  nome + " e tenho "  +  idade  + " anos");
+		System.out.println (numero1 + numero2  + " anos é a minha idade " + "= " + false); 
+		System.out.println ("***********************************");
+		
+		/* Escreva duas variáveis, uma para armazenar nome e a outra para armazenar sobrenome.
+		 * Ao imprimir, concatene as informações para apresentar o nome completo;
+		 * */
+		System.out.println ("Meu nome é: " + nome);
+		System.out.println ("Meu sobrenome é: " + sobrenome);
+		System.out.println ("Meu nome completo é: "  + nome  +  sobrenome); 
+		
+	
+		
+		
+		
+		
+		
+		
+		
+	
+		
+		
+		
+		
+
+	}
+
+}
