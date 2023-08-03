14a4a00fd763eb6868e3d27a86b570aac2329b0c
wjuliasouza <w.juliasouza@gmail.com>
2023-08-03 17:54:11 -0300

    Realizada atividade de criação de programa

---
+++ AtividadeCriacaoDePrograma/src/principal/AtividadeCriacaoDePrograma.java
@@ -0,0 +1,46 @@
+package principal;
+
+import java.util.Scanner;
+
+public class AtividadeCriacaoDePrograma {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		/*Faça um programa que peça um número e então mostre a mensagem O número informado foi (número informado)
+		 * Tente fazer a atividade anterior mas usando o comando de entrada de dados que aprendemos na aula de hoje 03/08/2023 (não use boolean
+		 */
+		
+		Scanner leia = new Scanner (System.in);
+		
+		double numero;
+		String nome;
+		
+		;
+		
+		
+		System.out.print("Informe um número: ");
+		numero = leia.nextDouble(); 
+		System.out.println ("O número informado foi: " + numero);
+		leia.nextLine();
+		
+		System.out.print("Informe o nome da sua mãe: ");
+		nome = leia.nextLine(); 
+		System.out.println ("O nome da minha mãe é: " + nome);
+		
+		
+		
+
+		
+		
+		
+		
+		
+		
+		
+		
+				;
+		
+	}
+
+}
